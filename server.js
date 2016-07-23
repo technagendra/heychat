9171d696af674ecca9f9445ed2ddc7121e6d320a
nagendra32 <nagendra.devireddy@gmail.com>
2016-07-23 16:37:53 +0530

    changes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(1500);
+}).listen(server_port,server_ip_address,function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:1500/');
 
