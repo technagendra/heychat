68f50d805fd7ecf98cb4c7a9584555c42aeada0d
nagendra32 <nagendra.devireddy@gmail.com>
2016-07-23 16:21:06 +0530

    making changes

---
+++ server.js
@@ -0,0 +1,19 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+var http = require("http");
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(1500);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:1500/');
+
